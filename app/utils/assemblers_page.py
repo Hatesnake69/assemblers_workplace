aeb1a70857ad28ad7a16e321a1c5dcaa65930f4b
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2023-10-25 18:32:01 +0300

    fix justify

--- app/utils/assemblers_page.py
+++ app/utils/assemblers_page.py
@@ -164,7 +164,7 @@ def fill_order_row(order: WbOrderModel):
         table += (
             f"<tr>"
             f"<td>{order.wb_id}</td>"
-            f"<td style='text-align: center !important;'><strong>КОМПЛЕКТ:</strong><br>{' + '.join([order_product.name for order_product in order_products])}</td>"
+            f"<td style='text-align: justify !important;'><strong>КОМПЛЕКТ:</strong><br>{' + '.join([order_product.name for order_product in order_products])}</td>"
             f"<td>{bundle_property}</td>"
             f"<td>{'-'}</td>"
             f"<td>{'-'}</td>"
