cac870ec912a13152349d347c26d98984a18b6b8
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2023-11-01 18:38:34 +0300

    fix

--- app/ip_whitelist_middleware.py
+++ app/ip_whitelist_middleware.py
@@ -9,7 +9,10 @@ class WhitelistMiddleware:
 
     def __call__(self, request):
         remote_ip = request.META.get('REMOTE_ADDR')
-        if remote_ip not in [elem.ip for elem in AllowedIpModel.objects.all()]:
+        print(remote_ip)
+        allowed_ips = [elem.ip for elem in AllowedIpModel.objects.all()]
+        print(allowed_ips)
+        if remote_ip not in allowed_ips:
             return HttpResponseForbidden("Access Denied")
 
         response = self.get_response(request)
