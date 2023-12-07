21b3810dfdbca2a34838fbedb142e662266aa99b
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2023-12-07 18:00:47 +0300

    fix response errors

--- app/services/api_request_service.py
+++ app/services/api_request_service.py
@@ -5,43 +5,54 @@ import requests
 
 
 class RequestAPI:
-    def get(self, url: str, headers: dict, params: dict, retries: int = 3):
+    def get(self, url: str, headers: dict, params: dict, retries: int = 3, timeout: int = 5):
         counter = retries
         while counter > 0:
             print(f"making get request to url: {url}")
-            response = requests.get(url=url, headers=headers, params=params)
-            if response.ok:
-                return response
+            try:
+                response = requests.get(url=url, headers=headers, params=params, timeout=timeout)
+                if response.ok:
+                    return response
+                else:
+                    print(f"error code: {response.status_code}, text: {response.text}")
+            except Exception as e:
+                print(f"exception : {e}")
             counter -= 1
             time.sleep(0.5)
-        raise Exception(f"{response.text}")
+        raise Exception(f"Возникла проблема при запросе к стороннему апи.")
 
-    def post(self, url: str, headers: dict, params: dict, body: dict, retries: int = 3):
+    def post(self, url: str, headers: dict, params: dict, body: dict, retries: int = 3, timeout: int = 5):
         counter = retries
         while counter > 0:
             print(f"making post request to url: {url}")
-            response = requests.post(url=url, headers=headers, params=params, json=body)
-            if response.ok:
-                return response
-            with open('error_log.txt', 'a') as file:
-                file.write(f"{response.text}\n")
+            try:
+                response = requests.post(url=url, headers=headers, params=params, json=body, timeout=timeout)
+                if response.ok:
+                    return response
+                else:
+                    print(f"error code: {response.status_code}, text: {response.text}")
+            except Exception as e:
+                print(f"exception : {e}")
             counter -= 1
             time.sleep(0.5)
-        raise Exception("website is not responding well")
+        raise Exception(f"Возникла проблема при запросе к стороннему апи.")
 
     def patch(
-        self, url: str, headers: dict, params: dict, body: dict, retries: int = 3
+        self, url: str, headers: dict, params: dict, body: dict, retries: int = 3, timeout: int = 5
     ):
         counter = retries
         while counter > 0:
             print(f"making post request to url: {url}")
-            response = requests.patch(
-                url=url, headers=headers, params=params, json=body
-            )
-            if response.ok:
-                return response
-            with open('error_log.txt', 'a') as file:
-                file.write(f"{response.text}\n")
+            try:
+                response = requests.patch(
+                    url=url, headers=headers, params=params, json=body, timeout=timeout
+                )
+                if response.ok:
+                    return response
+                else:
+                    print(f"error code: {response.status_code}, text: {response.text}")
+            except Exception as e:
+                print(f"exception : {e}")
             counter -= 1
             time.sleep(0.5)
-        raise Exception("website is not responding well")
+        raise Exception(f"Возникла проблема при запросе к стороннему апи.")
