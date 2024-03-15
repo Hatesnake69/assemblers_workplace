b7aaa7a54bfa8575a1b8bf2a41c1685742396f67
derijablya <dulugov@gmail.com>
2024-03-15 16:32:58 +0300

    minor refactoring

--- app/services/api_request_service.py
+++ app/services/api_request_service.py
@@ -1,62 +1,59 @@
 import time
-import traceback
 
 import requests
+from requests.adapters import HTTPAdapter, Retry
 
 
 class RequestAPI:
+    def __init__(self):
+        self._session = requests.Session()
+        retries = Retry(
+            total=3,
+            backoff_factor=0.5,
+        )
+        self._session.mount("http://", HTTPAdapter(max_retries=retries))
+        self._session.mount("https://", HTTPAdapter(max_retries=retries))
+
     def get(
-        self, url: str, headers: dict, params: dict, retries: int = 5, timeout: int = 5
+        self, url: str, headers: dict, params: dict, timeout: int = 5
     ):
-        counter = retries
-        while counter > 0:
-            print(f"making get request to url: {url}")
-            try:
-                response = requests.get(url=url, headers=headers, params=params, timeout=timeout)
-                if response.ok:
-                    return response
-                else:
-                    print(f"error code: {response.status_code}, text: {response.text}")
-            except Exception as e:
-                print(f"exception : {e}")
-            counter -= 1
-            time.sleep(0.5 + counter * 0.1)
+        print(f"making get request to url: {url}")
+        try:
+            response = self._session.get(url=url, headers=headers, params=params, timeout=timeout)
+            if response.ok:
+                return response
+            else:
+                print(f"error code: {response.status_code}, text: {response.text}")
+        except Exception as e:
+            print(f"exception : {e}")
         raise Exception(f"Возникла проблема при запросе к стороннему апи.")
 
     def post(
-        self, url: str, headers: dict, params: dict, body: dict, retries: int = 5, timeout: int = 5
+        self, url: str, headers: dict, params: dict, body: dict, timeout: int = 5
     ):
-        counter = retries
-        while counter > 0:
-            print(f"making post request to url: {url}")
-            try:
-                response = requests.post(url=url, headers=headers, params=params, json=body, timeout=timeout)
-                if response.ok:
-                    return response
-                else:
-                    print(f"error code: {response.status_code}, text: {response.text}")
-            except Exception as e:
-                print(f"exception : {e}")
-            counter -= 1
-            time.sleep(0.5 + counter * 0.1)
+        print(f"making post request to url: {url}")
+        try:
+            response = self._session.post(url=url, headers=headers, params=params, json=body, timeout=timeout)
+            if response.ok:
+                return response
+            else:
+                print(f"error code: {response.status_code}, text: {response.text}")
+        except Exception as e:
+            print(f"exception : {e}")
         raise Exception(f"Возникла проблема при запросе к стороннему апи.")
 
     def patch(
-        self, url: str, headers: dict, params: dict, body: dict, retries: int = 5, timeout: int = 5
+        self, url: str, headers: dict, params: dict, body: dict, timeout: int = 5
     ):
-        counter = retries
-        while counter > 0:
-            print(f"making patch request to url: {url}")
-            try:
-                response = requests.patch(
-                    url=url, headers=headers, params=params, json=body, timeout=timeout
-                )
-                if response.ok:
-                    return response
-                else:
-                    print(f"error code: {response.status_code}, text: {response.text}")
-            except Exception as e:
-                print(f"exception : {e}")
-            counter -= 1
-            time.sleep(0.5 + counter * 0.1)
+        print(f"making patch request to url: {url}")
+        try:
+            response = self._session.patch(
+                url=url, headers=headers, params=params, json=body, timeout=timeout
+            )
+            if response.ok:
+                return response
+            else:
+                print(f"error code: {response.status_code}, text: {response.text}")
+        except Exception as e:
+            print(f"exception : {e}")
         raise Exception(f"Возникла проблема при запросе к стороннему апи.")
